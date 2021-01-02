c6d444dfb31608f656f8230f0a10a6a905676e91
recelos15 <kubagr2000@gmail.com>
2021-01-03 00:30:08 +0100

    Naprawiane

--- Wielomian/src/com/company/Wielomian.java
+++ Wielomian/src/com/company/Wielomian.java
@@ -1,98 +1,104 @@
 package com.company;
+
+
 public class Wielomian {
+
     private int[] a;
-    public Wielomian(int[] tab){
+
+    private Wielomian(int[] tab) {
         a = new int[tab.length];
-        System.arraycopy(tab, 0, a,0,tab.length);
+        System.arraycopy(tab,0,a,0,tab.length);
     }
+    private Wielomian DodajWielomian(Wielomian input) {
+        Wielomian output = new Wielomian(new int[0]);
+        if (input.a.length == 0 && this.a.length == 0){
+            return output;
+        }
+        if (this.a.length == 0 || input.a.length == 0) {
+            if (this.a.length == 0) {
+                output.a = new int[input.a.length];
+                System.arraycopy(input.a, 0, output.a, 0, input.a.length);
+            } else {
+                output.a = new int[this.a.length];
+                System.arraycopy(this.a, 0, output.a, 0, a.length);
+            }
+            return output;
+        }
+        if (this.a.length == input.a.length) {
+            output.a = new int[a.length];
+            for (int i = 0; i < output.a.length; i++)
+                output.a[i] = this.a[i] + input.a[i];
+            return output;
+        }
 
-    public Wielomian DodajWielomian(Wielomian input) {
-        if (a.length == 0) {
-            Wielomian output = new Wielomian(new int[input.a.length]);
-            System.arraycopy(input.a,0 ,output.a,0, input.a.length);
-            return output;
-        }
-        if (input.a.length == 0) {
-            Wielomian output = new Wielomian(new int[a.length]);
-            System.arraycopy(a,0, output.a,0 ,a.length);
-            return output;
-        }
-
-        if(a.length==input.a.length){
-            Wielomian output = new Wielomian(new int[a.length]);
-            for (int i = 0; i<a.length;i++)
-                output.a[i] = a[i] + input.a[i];
-            return output;
-        }
         int shortLength, longLength;
 
-        if (a.length > input.a.length) {
-            longLength = a.length;
+        if (this.a.length > input.a.length) {
+            longLength = this.a.length;
             shortLength = input.a.length;
-        }
-        else {
+        } else {
             longLength = input.a.length;
-            shortLength = a.length;
+            shortLength = this.a.length;
         }
-
-        Wielomian output = new Wielomian(new int[longLength]);
+        output.a = new int[longLength];
         int i = 0;
         for (; i < shortLength; i++)
-            output.a[i] = a[i] + input.a[i];
+            output.a[i] = this.a[i] + input.a[i];
         for (; i < longLength; i++) {
-            if (a.length > input.a.length)
-                output.a[i] = a[i];
-            else if (a.length < input.a.length)
+            if (this.a.length > input.a.length)
+                output.a[i] = this.a[i];
+            else
                 output.a[i] = input.a[i];
         }
         return output;
     }
-    public Wielomian Pomnoz(int input) {
+
+    private void Pomnoz(int input) {
         for(int i = 0; i < a.length; i++)
             a[i] *= input;
-        return this;
-    }
-    public String ToString() {
-        if (a.length == 0)
-            return "0 * x^0";
-        String output = "";
-        for (int i = 0; i < a.length; i++) {
-            if (output == "" && a[i] != 0)
-                output += String.format("%d * x^%d",a[i],i);
-            else if (a[i] != 0)
-                output += String.format(" + %d * x^%d",a[i],i);
-        }
-        return output;
     }
 
+    @Override
+    public String toString() {
+        if (a.length == 0)
+            return "0 * x^0";
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < a.length; i++) {
+            if (output.toString().equals("") && a[i] != 0)
+                output.append(String.format("%d * x^%d", a[i], i));
+            else if (a[i] != 0)
+                output.append(String.format(" + %d * x^%d", a[i], i));
+        }
+        return output.toString();
+    }
     public static void Test() {
         // Czy puste tablice sa prawidlowo obslugowane? (ini i wyp)
         Wielomian w1 = new Wielomian(new int[0]);
-        System.out.println("ini w1: " + w1.ToString());
+        System.out.println("ini w1: " + w1);
 
         // Czy niepuste tablice sa prawidlowo obslugowane? (ini i wyp)
         int[] wsp1 = new int[] { 1, 2, 3, 4, 5 };
         Wielomian w2 = new Wielomian(wsp1);
-        System.out.println("ini w2: " + w2.ToString());
+        System.out.println("ini w2: " + w2);
 
         // Czy dziala mnozenie? Czy dziala wypisywanie ujemnych wspolczynnikow?
         w2.Pomnoz(-2);
-        System.out.println("mnz: w2 = " + w2.ToString());
+        System.out.println("mnz: w2 = " + w2);
 
         // Czy dziala dodawanie wielomianow roznej dlugosci?
         Wielomian w3 = w2.DodajWielomian(w1);
-        System.out.println("dod: w3 = w2 + w1 = " + w3.ToString());
+        System.out.println("dod: w3 = w2 + w1 = " + w3);
 
         // Czy konstruktor kopiuje tablice?
         for (int i = 0; i < wsp1.length; i++)
             wsp1[i] = -wsp1[i];
-        System.out.println("tst: w2 = " + w2.ToString());
+        System.out.println("tst: w2 = " + w2);
 
         // Czy dodawanie wielomianu samego do siebie nie powoduje bledow?
-        System.out.println("dod: w2 + w2 = " + w2.DodajWielomian(w2).ToString());
+        System.out.println("dod: w2 + w2 = " + w2.DodajWielomian(w2));
 
         // Czy dodawanie nie zmienia wielomianu?
-        System.out.println("tst: w2 = " + w2.ToString());
+        System.out.println("tst: w2 = " + w2);
     }
 }
 
